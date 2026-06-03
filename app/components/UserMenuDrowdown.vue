f7ac9c5dd6bd57265b3d74fd87c7906c5fef7a01
Mauricio Velazquez <mauri_i9070@hotmail.com>
2026-06-02 21:42:14 -0300

    feat: agregar componente UserMenuDropdown con opciones de perfil y agendar turno

---
+++ app/components/UserMenuDrowdown.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { DropdownMenuItem } from '@nuxt/ui'
+
+const { user } = useAuth()
+
+const userItems = computed<DropdownMenuItem[][]>(() => [
+  [
+    {
+      label: 'Mi Perfil',
+      icon: 'i-lucide-user',
+    },
+    {
+      label: 'Agendar Turno',
+      icon: 'i-lucide-calendar',
+    },
+  ],
+  [
+    {
+      label: 'Log out',
+      icon: 'i-lucide-log-out',
+    },
+  ],
+])
+</script>
+
+<template>
+  <UDropdownMenu
+    :items="userItems"
+    :content="{ align: 'center', collisionPadding: 12 }"
+    :ui="{ content: 'w-(--reka-dropdown-menu-trigger-width) min-w-48' }"
+  >
+    <UButton
+      v-bind="user"
+      :label="`${user?.firstName} ${user?.lastName}`"
+      trailing-icon="i-lucide-chevrons-up-down"
+      color="neutral"
+      variant="ghost"
+      square
+      class="w-full data-[state=open]:bg-elevated overflow-hidden"
+      :ui="{
+        trailingIcon: 'text-dimmed ms-auto',
+      }"
+    />
+  </UDropdownMenu>
+</template>
